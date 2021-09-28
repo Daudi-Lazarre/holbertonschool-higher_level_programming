0ebfd63182259e2dad7c297b50181bc11c64d38f
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-28 16:51:03 -0400

    Update 3-last_digit.py

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -7,8 +7,6 @@ if number > 0:
     positiveDigit = number % 10
     if positiveDigit > 5:
         print("Last digit of " + str(number) +  " is " + str(positiveDigit) + " and is greater than 5")
-    if positiveDigit == 0:
-        print("Last digit of " + str(number) +  " is " + str(positiveDigit) + " and is 0")
     if (positiveDigit < 6) & (positiveDigit != 0):
         print("Last digit of " + str(number) +  " is " + str(positiveDigit) + " and is less than 6 and not 0")
 
@@ -18,8 +16,6 @@ if number < 0:
     negativeDigit = number % -10
     if negativeDigit > 5:
         print("Last digit of " + str(number) +  " is " + str(negativeDigit) + " and is greater than 5")
-    if negativeDigit == 0:
-        print("Last digit of " + str(number) +  " is " + str(negativeDigit) + " and is 0")
     if (negativeDigit < 6) & (negativeDigit != 0):
         print("Last digit of " + str(number) +  " is " + str(negativeDigit) + " and is less than 6 and not 0")
 
