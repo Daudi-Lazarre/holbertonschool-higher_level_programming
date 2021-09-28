93f33be50b586bd3c69e6b4245092f48411d4d70
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-28 17:02:55 -0400

    Update 3-last_digit.py

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -6,7 +6,10 @@ number = random.randint(-10000, 10000)
 if number > 0:
     positiveDigit = number % 10
     if positiveDigit > 5:
-        print("Last digit of " + str(number) + " is " + str(positiveDigit) + " and is greater than 5")
+        firstPart = "Last digit of " + str(number) + " is "
+        secondPart = str(positiveDigit) + " and is greater than 5"
+        print(firstPart, secondPart)
+#        print("Last digit of " + str(number) + " is " + str(positiveDigit) + " and is greater than 5")
     if (positiveDigit < 6) & (positiveDigit != 0):
         print("Last digit of " + str(number) + " is " + str(positiveDigit) + " and is less than 6 and not 0")
 
