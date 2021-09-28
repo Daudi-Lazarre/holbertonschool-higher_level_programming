c56cc0d2d72894d4f6eee748e6ee628417a5100d
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-28 16:54:15 -0400

    Update 3-last_digit.py

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -6,18 +6,18 @@ number = random.randint(-10000, 10000)
 if number > 0:
     positiveDigit = number % 10
     if positiveDigit > 5:
-        print("Last digit of " + str(number) +  " is " + str(positiveDigit) + " and is greater than 5")
+        print("Last digit of " + str(number) + " is " + str(positiveDigit) + " and is greater than 5")
     if (positiveDigit < 6) & (positiveDigit != 0):
-        print("Last digit of " + str(number) +  " is " + str(positiveDigit) + " and is less than 6 and not 0")
+        print("Last digit of " + str(number) + " is " + str(positiveDigit) + " and is less than 6 and not 0")
 
 
 # Case for negative numbers: Converting the last number into a positive
 if number < 0:
     negativeDigit = number % -10
     if negativeDigit > 5:
-        print("Last digit of " + str(number) +  " is " + str(negativeDigit) + " and is greater than 5")
+        print("Last digit of " + str(number) + " is " + str(negativeDigit) + " and is greater than 5")
     if (negativeDigit < 6) & (negativeDigit != 0):
-        print("Last digit of " + str(number) +  " is " + str(negativeDigit) + " and is less than 6 and not 0")
+        print("Last digit of " + str(number) + " is " + str(negativeDigit) + " and is less than 6 and not 0")
 
 if number == 0:
-    print("Last digit of " + str(number) +  " is " + str(number) + " and is 0")
+    print("Last digit of " + str(number) + " is " + str(number) + " and is 0")
