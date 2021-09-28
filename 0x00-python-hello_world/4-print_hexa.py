0d33c0b215f61cc4349bc62d423c0af84d1d52aa
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-28 18:39:43 -0400

    Update 4-print_hexa.py

--- 0x00-python-hello_world/4-print_hexa.py
+++ 0x00-python-hello_world/4-print_hexa.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-# First curly braces are for the regular numbers
-# Second curly braces are for the hexadecimal numbers
-
 for number in range(1,99):
     print("{} = {}".format(number, hex(number)))
