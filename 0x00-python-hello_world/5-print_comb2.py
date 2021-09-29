cf09e2a2ce9ae1456431f52833a7c7f6a1d9bc81
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-28 20:34:29 -0400

    Add: Files, number 8

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
+# SMH, I want to use a list comprehension.
+# I can't though, because Holberton.
+# oneToNinetyNine = [num for num in range(1, 99)]
+# print(oneToNinetyNine)
+
+# For the numbers from 0 to 99
+# Print, using two places, the numbers
+# end the printed statement with a comma and space
+# Then, once that is done, print the number 99
+
 for num in range(99):
     print("{:02d}".format(num), end=", ")
 print("{:02d}".format(num + 1))
