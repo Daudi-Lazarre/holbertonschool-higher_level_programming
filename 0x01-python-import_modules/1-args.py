2242f2833d7faa493edecf4da2602d8e25f04a37
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-29 19:43:03 -0400

    Update 1-args.py

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -10,13 +10,13 @@ def main(*argv):
         if (i != 0):
             print("{}: {}".format(i, args))
             i = i + 1
-    # Take this out of the for loop?
-        if total == 1:
-            print("{:d} argument:".format(total))
-        elif total == 0:
-            print("{:d} argument:".format(total))
-        else:
-            print("{:d} argument:".format(total))
+
+    if total == 1:
+        print("{:d} argument:".format(total))
+    elif total == 0:
+        print("{:d} argument:".format(total))
+    else:
+        print("{:d} argument:".format(total))
 
 if __name__ == "__main__":
     main()
