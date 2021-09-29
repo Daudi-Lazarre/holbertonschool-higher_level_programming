e540479effad29de7857457c3de1453218b408fd
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-28 20:59:46 -0400

    Update 6-print_comb3.py

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
+# I'm not sure I understand this at the moment, but hey...
+# Ask C15 about this tomorrow
+
+# MY UNDERSTANDING:
+# Increment through 0-8
+# and also (first + 1) up until the the number 9
+# print both digits with a comma and space
+# print 89, but not 99 
+
 for first in range(0, 8):
     for second in range(first + 1, 10):
         print("{:d}{:d}".format(first, second), end=', ')
-# print("{:d}{:d}".format(first + 1, second))
+print("{:d}{:d}".format(first + 1, second))
