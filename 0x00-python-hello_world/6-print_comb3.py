87848852b5fa5171f713ea01558790f21ec26395
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-28 20:58:51 -0400

    Update 6-print_comb3.py

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -3,4 +3,4 @@
 for first in range(0, 8):
     for second in range(first + 1, 10):
         print("{:d}{:d}".format(first, second), end=', ')
-print("{:d}{:d}".format(first + 1, second))
+# print("{:d}{:d}".format(first + 1, second))
