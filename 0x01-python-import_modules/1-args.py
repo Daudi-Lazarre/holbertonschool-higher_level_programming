36db2f7c0070a14f0e35b8a7f362c1d84b720cf7
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-30 12:46:17 -0400

    Update 1-args.py

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -6,16 +6,17 @@ def main(*argv):
     i = 0
     total = len(sys.argv)
 
+    if total == 1:
+        print("{:d} arguments:".format(total - 1))
+    elif total == 2:
+        print("{:d} argument:".format(total - 1))
+    else:
+        print("{:d} arguments:".format(total - 1))
+
     for args in sys.argv:
+        if (i != 0):
             print("{}: {}".format(i, args))
             i = i + 1
-
-    if total == 1:
-        print("{:d} argument:".format(total))
-    elif total == 0:
-        print("{:d} argument:".format(total))
-    else:
-        print("{:d} argument:".format(total))
 
 if __name__ == "__main__":
     main()
