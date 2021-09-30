827d5a1e77942a457c203ce10ba12e564f414312
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-30 13:04:20 -0400

    Update 1-args.py

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -6,19 +6,12 @@ def main(*argv):
     i = 0
     total = len(sys.argv)
 
-# I over-hyped this problem.
-# Do total - 1 to shift numbers back to start at zero.
-# ... because total does not start a 0th index???
-
     if total == 1:
         print("{:d} arguments:".format(total - 1))
     elif total == 2:
         print("{:d} argument:".format(total - 1))
     else:
         print("{:d} arguments:".format(total - 1))
-
-# Nvm, why is this printing the file and not the output after it?
-# I was printing the wrong variable
 
     for arg in range(1, total):
             print("{}: {}".format(arg, sys.argv[arg]))
