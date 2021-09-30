1682a1e4c0956491ecd78f3f740e33b6a8876b8e
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-09-30 16:22:04 -0400

    Add: File 2-3 changes

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        ouput = a / b
+    
+    except ZeroDivisionError:
+        output = None
+
+    finally:
+        print("Inside result: {}".format(output))
+    return output
