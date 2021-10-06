1a6def07b22cc1c10595ea3d6778f7bdbd42809a
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-05 23:46:13 -0400

    Update 0-square.py

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# 0-square.py
 
 '''0-rectangle: Python Function that defines an empty class'''
 
 
 class Rectangle:
     '''Create an empty Rectangle type'''
-        def __init__(self, size):
-        self.__size = size
+    pass
