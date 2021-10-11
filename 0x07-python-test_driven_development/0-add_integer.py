03620132c2a9d3aa36af422ed93bb8e716cd4630
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-11 15:21:47 -0400

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-    return(a + b)
 
-# Check to see if a and b are integers or floats
-# If not, raise a typerror.
+def add_integer(a, b=98):
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return(a + b)
