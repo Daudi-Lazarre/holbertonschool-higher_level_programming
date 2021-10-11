7c913f8616d6eaf9d1f07ffb869d172218119bf7
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-11 18:30:42 -0400

    Add: Task 1, not finished though

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    matrix = []
+
+    # Append these bad boys
+    row_one = [1, 2, 3]
+    row_two = [4, 5, 6]
+
+    matrix.append(row_one)
+    matrix.append(row_two)
+
+    # Does not row one and row two 
+    if not isinstance(row_one, (int, float)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(row_two, (int, float)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if len(row_one) != len(row_two):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    div = ()
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    quotient = matrix / div
+
+    return(round(quotient, 2))
