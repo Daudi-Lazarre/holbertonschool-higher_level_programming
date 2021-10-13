f2c2428bfc1452c6687cccb17bc213d2eb27d90c
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-13 13:51:59 -0400

    Add: Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Lookup: Returns a list of the available attributes and methods of an obj. """
+
+
+def lookup(obj):
+    """ The available attributes and methods of the obj. """
+
+# dir() returns the lists and attributes of any object
+    return dir(obj)
