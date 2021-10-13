af0b7386625b185f1aa1b86bdd33ad1c897becb6
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-13 14:22:09 -0400

    Add: Task 1, haven't tested it yet

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" MyList: inherits stuff from list """
+
+class MyList(list):
+    """ MyList, with the stuff inherited from the list """
+
+    def print_sorted(self):
+        """ Print the sorted list """
+        print(sorted(self))
