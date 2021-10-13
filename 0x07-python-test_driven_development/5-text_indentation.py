5a8a235bed99a4b048bd5d8568e36c4c816c3422
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-12 23:58:14 -0400

    Add: Task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" Text Indentation """
+
+
+def text_indentation(text):
+    """ Function that indents text """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    newtext = text.replace('?', '?\n\n')
+    newtext = newtext.replace('.', '.\n\n')
+    newtext = newtext.replace(':', ':\n\n')
+    print("\n".join([item.strip() for item in newtext.split("\n")]), end="")
