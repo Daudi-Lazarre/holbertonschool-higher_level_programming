9c68cf8e7104723653cee40039cf0b707e71a4b6
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-13 22:32:28 -0400

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -2,6 +2,7 @@
 """ BaseGeometry: returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.
 """
 
+
 class BaseGeometry:
     """ empty class """
     pass
