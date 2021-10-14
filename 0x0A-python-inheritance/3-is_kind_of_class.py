8a17e69b9ccc3f43f0b0dfa1c76ac51754b25820
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-13 21:21:04 -0400

    Add: Files

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,4 +6,5 @@ is_kind_of_class: returns True if the object is an instance of, or if the object
 
 def is_kind_of_class(obj, a_class):
     """ the object is an instance """
+
     return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,5 @@ inherits_from: returns True if the object is an instance of a class that inherit
 
 def inherits_from(obj, a_class):
     """ the object is or isn't a class: Returns True and False """
+
     return isinstance(obj, a_class) and type(obj) != a_class
