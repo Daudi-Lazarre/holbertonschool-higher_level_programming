db33cf4b9e3ea99d665d268f685fd66188317a10
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-14 18:07:34 -0400

    Update 3-square.py

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Square class """
+
+
+class Square():
+    """ Square """
+
+    def __init__(self, size=0):
+        """ What's the size? """
+
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Square area """
+
+        return self.__size ** 2
