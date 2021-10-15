32ccaedcc6c128a3e6dbf95713dc40f8e1f46ea4
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-15 18:03:31 -0400

    Add: Task 2, among other things
    Task 2 doesn't work as is. Thinking of naming file.write()

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" appends a string at the end of a text file (UTF8) and returns the number of characters added: """
+
+
+def append_write(filename="", text=""):
+    """ must use with statement here """
+
+    with open(filename, mode='a', encoding="UTF-8") as file:
+        return(file.write(file))
