48399d525cbff0006cd28ac843b8c5f0f7358b8b
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-15 18:30:46 -0400

    Update 4-from_json_string.py
    The opposite of task 3: Didn't even check it, tbh.

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" returns an object (Python data structure) represented by a JSON string: """
+import json
+
+
+def from_json_string(my_str):
+    """ From Rob Jecque'd to Jason """
+
+    return(json.loads(my_str))
