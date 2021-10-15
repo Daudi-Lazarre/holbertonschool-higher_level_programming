76c39c5b5a09fe38a9ad34dda89d119b89771033
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-15 19:05:18 -0400

    Update 6-load_from_json_file.py
    There was a lot of debugging for this, since I copied and pasted the previous algo. This should be everything, though.

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ creates an Object from a “JSON file”: """
-
-
 import json
+
 
 def load_from_json_file(filename):
     """ From Jason to object FILE """
     
     with open(filename) as file:
-        j_to_obj = json.load(filename)
+        j_to_obj = json.load(file)
         return(j_to_obj)
