56df351b94157d07051a33f8dd748ad2255e2510
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-15 18:45:46 -0400

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/python3#!/usr/bin/python3
+""" writes an Object to a text file, using a JSON representation: """
+
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """ obj to txt file  """
+    
+    with open(filename, mode='a', encoding="UTF-8") as file:
+        save_Jason = json.loads(my_obj)
+        file.write(save_Jason)
