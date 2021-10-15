18791981e42733c399605b920a46589c28f7234a
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-15 15:25:02 -0400

    Add: Task 0
    read() and readline() does make a difference. readline() will reach each character typed in including newline breaks, whereas read() will interpret the contents of the file similarly to how a human would.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Reads a text file (UTF8) and prints it to stdout """
+
+
+def read_file(filename=""):
+    """ must use with statement here """
+
+    with open(filename, encoding="UTF-8") as file:
+        print(filename)
+        read_the_line = file.read()
+        print(read_the_line, end="")
