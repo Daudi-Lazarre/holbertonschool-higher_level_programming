0abcba99aced8b92865ac7853b5961842c7efeae
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-15 18:22:45 -0400

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" returns the JSON representation of an object (string): """
+import json
+
+
+def to_json_string(my_obj):
+    """ From Jason to Rob Jecque'd """
+
+    return(json.dumps(my_obj))
