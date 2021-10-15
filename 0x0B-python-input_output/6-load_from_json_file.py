2f0b9fc82953d64584f177dce74861dcceb61ae7
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-15 18:51:37 -0400

    Update 6-load_from_json_file.py
    I should probably use the correct module name, haha

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3#!/usr/bin/python3
-""" writes an Object to a text file, using a JSON representation: """
+""" creates an Object from a “JSON file”: """
 
 
 import json
 
 def save_to_json_file(my_obj, filename):
-    """ obj to txt file  """
+    """ def load_from_json_file(filename): """
     
     with open(filename, mode='w', encoding="UTF-8") as file:
         save_Jason = json.loads(my_obj)
         file.write(save_Jason)
+
+
+# You don’t need to manage exceptions if the JSON string doesn’t represent an object.
+# You don’t need to manage file permissions / exceptions.
