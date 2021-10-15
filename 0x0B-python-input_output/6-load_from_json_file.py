7a88b82f047777d6b80601a4538d6fdf7b2ccb47
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-15 18:53:06 -0400

    Update 6-load_from_json_file.py
    Return the object, let's see if this works.

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,11 +5,12 @@
 import json
 
 def save_to_json_file(my_obj, filename):
-    """ def load_from_json_file(filename): """
+    """ From Jason to object FILE """
     
     with open(filename, mode='w', encoding="UTF-8") as file:
         save_Jason = json.loads(my_obj)
         file.write(save_Jason)
+        return(save_Jason)
 
 
 # You don’t need to manage exceptions if the JSON string doesn’t represent an object.
