a98cbe70e43d82a9c7955d00f736967831d037da
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-15 19:32:18 -0400

    Update 9-student.py
    PEP8!

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,10 +6,13 @@ class Student:
     
     def __init__(self, first_name, last_name, age):
         """ Here's the student info: name, age """
-
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
 
 
 
     def to_json(self):
         """ Retrieves a dictionary """
+        self.__dict__
