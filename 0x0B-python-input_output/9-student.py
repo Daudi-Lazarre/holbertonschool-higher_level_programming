455184cd0120aadd3ed7b6ef9db1c7d9f9ed0160
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-15 19:30:07 -0400

    Update 9-student.py
    Not done yet!

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+""" Write a class Student that defines a student by: """
+
+class Student:
+    """ Contains Student information """
+    
+    def __init__(self, first_name, last_name, age):
+        """ Here's the student info: name, age """
+
+
+
+
+
+    def to_json(self):
+        """ Retrieves a dictionary """
