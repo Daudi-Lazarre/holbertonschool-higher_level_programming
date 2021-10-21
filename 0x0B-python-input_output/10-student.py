2e333f589f237a027abcfa2c2df5053fe3102b41
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-21 14:56:31 -0400

    Update 10-student.py
    Me: *makes change to file*
    
    Also me: "is this PEP8 standards?"

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,12 +7,15 @@ class Student:
     
     def __init__(self, first_name, last_name, age):
         """ Here's the student info: name, age """
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """ Retrieves a dictionary """
+
+
         if attrs is not None:
             new_Dictionary = {}
             for i in attrs:
