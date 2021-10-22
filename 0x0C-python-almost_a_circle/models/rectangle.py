c21832c8f3ad5da8619a2ae241c8c3ea2813651c
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-21 21:12:34 -0400

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,3 +13,31 @@ class Base:
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """ Set the width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Set the width """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+                raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Remember: Decorators allow for attributes to be modified """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Set the height """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
