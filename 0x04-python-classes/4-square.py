58cb08b49db7f067f65fc88a4f066c41ce539d0c
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-23 07:39:17 -0400

    Update 4-square.py

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Define Square by size """
+
+
+class Square:
+    """Define size with verification"""
+
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    """Define area of square"""
+
+    def area(self, size=0):
+        sq_area = self.__size ** 2
+        return sq_area
+
+    """ Retrieve size """
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Get the size """
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """ Print the things """
+    def my_print(self):
+        size = self.__size
+        if type(size) is not 0:
+            for key in range(size):
+                for key in range(size):
+                    print("#", end="")
+                print()
+        else:
+            print("")
