573eab170328e33f80e2e952cf2c074b44928fcb
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-23 14:32:49 -0400

    Create square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Square file """
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ initalize """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ Size decorator """
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width, self.height = size
+
+    def update(self, *args, **kwargs):
+        """ Assigns argument to each attribute. """
+        if args:
+            attributes = ("id", "width", "height", "x", "y")
+            for attribute in range(0, len(args)):
+                setattr(self, attributes[attribute], args[attribute])
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+
+
+    def __str__(self):
+        """ overriding the __str__ method """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id,
+            self.x,
+            self.y,
+            self.width,
+            self.height,
+        )
