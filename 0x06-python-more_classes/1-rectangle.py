57a3bfcfd1b7add27e631b4b35c8f27d97f54afa
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-10-23 11:44:49 -0400

    Update 1-rectangle.py

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+""" Rectangle class """
+
+
+class Rectangle:
+    """ Get Rekt """
+
+    def __init__(self, width=0, height=0):
+        """ * Launching rectangle into existence * """
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """ Remember: Decorators allow for attributes to be modified """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Set the height """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """ Set the width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Set the width """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+                raise ValueError("width must be >= 0")
+        self.__width = value
