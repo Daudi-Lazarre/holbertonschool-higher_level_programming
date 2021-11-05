839e4c4f48f3ddcce5df8a27b9100b4ba4ebeba6
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-11-05 17:46:09 -0400

    Update 12-no_genre.sql

--- 0x01-SQL_more_queries/12-no_genre.sql
+++ 0x01-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- lists all shows without the genre
-SELECT a.title, b.genre_id
-FROM tv_shows a
-LEFT JOIN tv_show_genres b
-ON a.id = b.show_id
-WHERE b.genre_id IS NULL
-ORDER BY a.title ASC, b.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows
+LEFT JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id 
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
