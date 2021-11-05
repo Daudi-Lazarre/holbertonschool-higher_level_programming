c6ca04fe37887978a03c4f4eb08fe901fe55e6a8
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-11-05 18:12:22 -0400

    Update 1-rectangle.py

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -1,42 +1,39 @@
 #!/usr/bin/python3
-""" Class Rectangle """
+""" Rectangle class """
 
 
 class Rectangle:
-    """ method initialization """
+    """ Get Rekt """
 
     def __init__(self, width=0, height=0):
-        """ defines a rectangle """
-
-        self.width = width
+        """ * Launching rectangle into existence * """
         self.height = height
-
-    @property
-    def width(self):
-        """property decorator"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Setter and value validation"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.width = width
 
     @property
     def height(self):
-        """property decorator"""
+        """ Remember: Decorators allow for attributes to be modified """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter and value validation"""
-        if not isinstance(value, int):
+        """ Set the height """
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
+
+    @property
+    def width(self):
+        """ Set the width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Set the width """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+                raise ValueError("width must be >= 0")
+        self.__width = value
