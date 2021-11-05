1f86b03c85765d6fd537c58706ef5dd89181abf6
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2021-11-05 17:42:29 -0400

    Update 16-shows_by_genre.sql

--- 0x01-SQL_more_queries/16-shows_by_genre.sql
+++ 0x01-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Lists show and genre
 SELECT tv_shows.title title, tv_genres.name 
 FROM tv_shows
-JOIN tv_show_genres 
+LEFT OUTER JOIN tv_show_genres 
 ON tv_shows.id = tv_show_genres.show_id 
-JOIN tv_genres 
+LEFT OUTER JOIN tv_genres 
 ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name ASC;
