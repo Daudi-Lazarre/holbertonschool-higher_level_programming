56d12fdeead26db0d392acca9bbf2b68ae99d0c5
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-06 23:22:15 -0500

    Update 7-multi_c.js

---
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const val = Number(process.argv[2]);
+
+if (isNaN(val)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < val; i++) {
+    console.log("C is fun");
+  }
+}
