43fe2394195def0266efa75296ec74aa5da73926
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-06 22:40:59 -0500

    Update 2-arguments.js

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
 
-// Write a script that prints a message depending of the number of arguments passed:
-
-// if arg == 0
-// print "No argument"
-// else if arg == 1 
-// print "Argument found"
-// else "Arguments found"
-
-
 const value = argv.length;
 
 if (value < 3) {
@@ -18,4 +9,3 @@ if (value < 3) {
   } else {
     console.log('Arguments found');
   }
-
