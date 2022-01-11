2ff2079b74289e9bd2086af85cca2ccd7a0b1643
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-11 16:29:50 -0500

    Create 0-readme.js

---
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const argv = process.argv;
+const file = argv[2];
+
+fs.readFile(filename, 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err)
+    }
+    process.stdout.write(data);
+});
