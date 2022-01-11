6b10e0f0c0dde6b46b3cfc606b63e4444dff1e82
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-11 16:53:51 -0500

    Create 6-completed_tasks.js

---
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+const URL = argv[2];
+const request = require('request');
+
+request(URL, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } 
+  else {
+    const rbody = JSON.parse(body);
+    const dictionary = {};
+    for (const x of rbody) {
+      if (x.completed === true) {
+        if (dictionary[x.userId] === undefined) {
+          dictionary[x.userId] = 0;
+        }
+        dictionary[x.userId] += 1;
+      }
+    }
+    console.log(dictionary);
+  }
+});
