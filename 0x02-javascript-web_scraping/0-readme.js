20160a206586b10967a7f878e2c6d39091d8cc9e
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-11 20:10:56 -0500

    Update 0-readme.js

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const argv = process.argv;
 const file = argv[2];
 
-fs.readFile(filename, 'utf8', function (err, data) {
+fs.readFile(file, 'utf8', function (err, data) {
     if (err) {
         return console.log(err)
     }
