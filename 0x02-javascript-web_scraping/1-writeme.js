02c6c867abae9a69f960eece1813375c80c8c4ee
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-11 20:13:32 -0500

    Update 1-writeme.js

--- 0x02-javascript-web_scraping/1-writeme.js
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const argv = process.argv;
 const file = argv[2];
 const string = argv[3];
 
-fs.writeFile(filename, string, 'utf8', function (err) {
+fs.writeFile(file, string, 'utf8', function (err) {
     if (err) {
         return console.log(err)
     }
