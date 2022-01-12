9566271d6b50a8d9007e8f7c309831548b4a5813
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-11 20:31:43 -0500

    Update 3-starwars_title.js

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const whatMovie = argv[2];
-const URL = 'https://swapi-api.hbtn.io/api/films/:id' + whatMovie[2] + '/';
+const URL = 'https://swapi-api.hbtn.io/api/films/:id' + whatMovie + '/';
 
 request(URL, function (err, response, body) {
     if (err) {
