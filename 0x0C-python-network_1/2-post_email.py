7d0ae11bcaa860eba1dd7c9f980aae5db58f33ff
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-26 12:54:04 -0500

    Update 2-post_email.py

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     email = sys.argv[2]
-    encode = urllib.parse.urlencode({"email": email}).encode('ascii')
+    encode = urllib.parse.urlencode({"email": email}).encode("ascii")
     with urllib.request.urlopen(sys.argv[1], encode) as email:
         s = email.read()
         print(s.decode("utf-8"))
