bfd92107d149bc9eba900cd8b30fb830c7920cfd
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-26 13:41:35 -0500

    Update 3-error_code.py

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
 
     except ValueError as message:
         message = urllib.error.HTTPError
-        print("Error code: {}".format(message))
+        print("Error code: {}".format(message.code))
