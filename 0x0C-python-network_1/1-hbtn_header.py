2a0547656852403e9adea363665e6cbc9241358d
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-26 12:19:50 -0500

    Update 1-hbtn_header.py
    Changed var name

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -5,6 +5,5 @@ import sys
 
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as f:
-        myHeader = f.getheader("X-Request-Id")
-        print("{}".format(myHeader))
-        
+        header = f.getheader("X-Request-Id")
+        print("{}".format(header))
