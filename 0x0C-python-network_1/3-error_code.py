e7878f8b5ec3025618acd1f9f79a9a810292d293
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-26 13:17:13 -0500

    Update 3-error_code.py

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as url:
         s = url.read()
-        
-        print(s.decode("utf-8"))
+
         message = urllib.error.HTTPError
+        print(s.decode("utf-8"))
         print("Error code: {}".format(message))
