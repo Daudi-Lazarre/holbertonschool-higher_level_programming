d212c4325740f71d72f12f57285aed5aae88327e
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-26 14:08:42 -0500

    Update 4-hbtn_status.py

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 import requests
 
-with requests.fetch as f:
-    fetch = requests.get("https://intranet.hbtn.io/status")
-    myFile = f.read()
+fetch = requests.get("https://intranet.hbtn.io/status")
 
 print("Body response:")
-print("\t- type: {}".format(type(myFile)))
-print("\t- content: {}".format(myFile))
-print("\t- utf8 content: {}".format((myFile.decode("utf-8"))))
+print("\t- type: {}".format(type(fetch)))
+print("\t- content: {}".format(fetch))
