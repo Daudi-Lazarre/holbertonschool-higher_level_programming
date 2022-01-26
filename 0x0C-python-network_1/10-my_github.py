fe9e219fac7895f2c5a82e17b4524198dcae2825
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-26 17:40:11 -0500

    Update 10-my_github.py

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
 
-    req = requests.get("https://api.github.com/user", 
+    req = requests.get("https://api.github.com/users", 
                         profile=(username, password))
 
     print(req.json().get("id"))
