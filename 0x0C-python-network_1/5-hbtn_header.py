98fe04597ea31d89ac4f9cc31c105c87f0fd308c
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-26 14:30:44 -0500

    Update 5-hbtn_header.py

--- 0x0C-python-network_1/5-hbtn_header.py
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -7,4 +7,4 @@ import sys
 
 
 if __name__ == "__main__":
-        print("{}".format(requests.get(sys.argv[1]).headers.get("X-Request-Id")))
+    print("{}".format(requests.get(sys.argv[1]).headers.get("X-Request-Id")))
