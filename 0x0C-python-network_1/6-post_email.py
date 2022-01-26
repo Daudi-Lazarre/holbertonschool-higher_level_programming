dbc247c2ed418025a85de77c2a38d0235691414a
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-26 14:42:20 -0500

    Update 6-post_email.py

---
+++ 0x0C-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""How to post an email"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+   
+    email = sys.argv[2]
+    req = requests.post(sys.argv[1], data={"email": email})
+    print(req.text)
