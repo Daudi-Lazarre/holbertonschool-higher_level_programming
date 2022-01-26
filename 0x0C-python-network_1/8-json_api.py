91fb66dab25ad4e1d66b867fb6e40f39b1b47419
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-26 17:01:26 -0500

    Update 8-json_api.py

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""How to post an email"""
+
+import requests
+import sys
+
+q = sys.argv[1]
+
+if __name__ == "__main__":
+    if len(q) < 1:
+        data = {"q": ""}
+    elif len(q) > 1:
+        data = sys.argv[1]
+
+    try:
+        url = requests.post("http://0.0.0.0:5000/search_user", data)
+
+        if url.json():
+            print("[{}] {}".format(url.json().get("id"),
+                                    url.json().get("name")))
+
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
