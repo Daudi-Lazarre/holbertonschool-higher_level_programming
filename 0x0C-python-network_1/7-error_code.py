f42d57a331ead4e5fd0deb3b69e8cdff8669f745
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-26 15:08:16 -0500

    Update 7-error_code.py

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -7,7 +7,7 @@ from requests.exceptions import HTTPError
 
 if __name__ == "__main__":
     req = requests.get(sys.argv[1])
-        
+
     if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
 
