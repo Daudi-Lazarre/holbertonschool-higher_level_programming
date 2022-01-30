05f37e78230027207acd90cff07d3ee76b99fa2e
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-29 20:30:46 -0500

    Update 3-rectangle.js

--- 0x01-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,7 +6,7 @@ class Rectangle {
       this.height = h;
     }
   }
-  print () {
+  print() {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
         process.stdout.write('X');
@@ -16,5 +16,5 @@ class Rectangle {
       }
     }
   }
-};
+}
 module.exports = Rectangle;
