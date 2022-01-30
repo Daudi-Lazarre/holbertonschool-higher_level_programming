4f9315b2631b7b153d08cfc9b70101080e458e6a
Daudi Lazarre <77812330+Daudi-Lazarre@users.noreply.github.com>
2022-01-29 20:15:45 -0500

    Update 7-occurences.js

--- 0x01-javascript_objects_scopes_closures/7-occurences.js
+++ 0x01-javascript_objects_scopes_closures/7-occurences.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
+
 exports.nbOccurences = function (list, searchElement) {
-    return list.filter(item => item === searchElement).length;
-  };
+  let x = 0;
+
+  for (const item of list) {
+    if (item === searchElement) {
+      x++;
+    }
+  }
+
+  return x;
+};
