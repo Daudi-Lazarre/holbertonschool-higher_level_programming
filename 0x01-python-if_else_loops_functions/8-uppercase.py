c6657183347744204ee9726d8d314b9366f97960
Daudi Lazarre <lucaslazarre@DaudiLazarre.local>
2022-05-06 18:55:34 -0400

    Create 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        letterInString = ord(letter)
+        if (letterInString >= 97) and (letterInString <= 122):
+            letter = chr(letterInString - 32)
+            print(letter, end="")
+        print("\n", end="")
