ffd7ad767a2dc1e5eed1b4ff559a0ae3973f66b3
Daudi Lazarre <lucaslazarre@DaudiLazarre.local>
2022-05-09 13:33:37 -0400

    Update 12-roman_to_int.py

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
     romanNumeral = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    
+
     output = 0
     index = 1
 
-    if not roman_string or roman_string != str:
+    if not roman_string:
+        return 0
+    elif type(roman_string) != str:
         return 0
     if len(roman_string) == 1:
         return (romanNumeral[roman_string[0]])
@@ -14,6 +17,6 @@ def roman_to_int(roman_string):
             output += (romanNumeral[roman_string[index - 1]] * -1)
         else:
             output += romanNumeral[roman_string[index - 1]]
-
+    
     output += romanNumeral[roman_string[index]]
     return output
