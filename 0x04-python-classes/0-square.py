fe33d52bcf73b277806b0085976e4ab2cb2a9a14
Daudi Lazarre <lucaslazarre@DaudiLazarre.local>
2022-05-10 12:04:45 -0400

    Update 0-square.py

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,7 +1,9 @@
+
 #!/usr/bin/python3
 '''0-rectangle: Python Function that defines an empty class'''
 
 class Square:
+
     """LEAVE ME HERE"""
     def __init__(self, size):
         self.__size = size
