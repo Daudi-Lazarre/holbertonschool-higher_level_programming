0578edc24114b8e7f7bb90fe853ab8d8e5a5267a
Daudi Lazarre <lucaslazarre@DaudiLazarre.local>
2022-05-16 10:14:51 -0400

    Update 1-rectangle.py

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -1,39 +1,51 @@
 #!/usr/bin/python3
-""" Rectangle class """
+"""
+    File: 1-rectangle.py
+"""
 
 
-class Rectangle:
-    """ Get Rekt """
-
+class Rectangle(object):
+    """rectangle class"""
     def __init__(self, width=0, height=0):
-        """ * Launching rectangle into existence * """
-        self.height = height
+        """Initialize rectangle"""
         self.width = width
-
-    @property
-    def height(self):
-        """ Remember: Decorators allow for attributes to be modified """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ Set the height """
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+        self.height = height
 
     @property
     def width(self):
-        """ Set the width """
+        """getter for  width."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Set the width """
-        if type(value) is not int:
+        """setter for width"""
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-                raise ValueError("width must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """getter forheight."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """return area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """return perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((2 * self.__width) + (2 * self.__height))
