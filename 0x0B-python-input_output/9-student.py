74dff7c134feb0d5a597c7e3fc6fadfee0a607a8
Daudi Lazarre <lucaslazarre@DaudiLazarre.local>
2022-05-20 17:09:53 -0400

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-""" Write a class Student that defines a student by: """
+"""
+    Student file
+"""
 
 
-class Student:
-    """ Contains Student information """
-    
+class Student(object):
+    """Student"""
     def __init__(self, first_name, last_name, age):
-        """ Here's the student info: name, age """
+        """__init__"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """ Retrieves a dictionary """
-        self.__dict__
-
+        """returns a dictionary representation """
         return self.__dict__
