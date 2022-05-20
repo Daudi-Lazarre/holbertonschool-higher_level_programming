b60d6aec40a3622641e1cef44b81a3d1381c14c5
Daudi Lazarre <lucaslazarre@DaudiLazarre.local>
2022-05-20 17:11:46 -0400

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-""" Write a class Student that defines a student by: (based on 9-student.py) """
+"""
+    Student file
+"""
 
 
-class Student:
-    """ Contains Student information """
-    
+class Student(object):
+    """Student"""
     def __init__(self, first_name, last_name, age):
-        """ Here's the student info: name, age """
-
+        """__init__"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ Retrieves a dictionary """
-
-
-        if attrs is not None:
-            new_Dictionary = {}
-            for i in attrs:
-                if i in self.__dict__:
-                    new_Dictionary[i] = self.__dict__[i]
-            return(new_Dictionary)
-        else:
+        """returns a dictionary representation """
+        if attrs is None:
             return self.__dict__
+        diction = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                diction[key] = value
+        return diction
 
     def reload_from_json(self, json):
-        """ What is a student? """
+        """reload_from_json"""
         for key, value in json.items():
-            self.__dict__[key] = value
+            setattr(self, key, value)
