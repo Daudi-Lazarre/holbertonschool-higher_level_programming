a2a0f0e36b14f5bb3d240bc47135ae1e4052136d
Daudi Lazarre <lucaslazarre@DaudiLazarre.local>
2022-05-26 22:38:02 -0400

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,42 +1,68 @@
 #!/usr/bin/python3
-""" Square file """
+"""
+    File: Square.py
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Square class, import some attrs from rectangle """
+    """Square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ initalize """
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """__str__ method"""
+        return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y,
+                                                 self.width)
 
     @property
     def size(self):
-        """ Size decorator """
+        """size getter"""
         return self.width
 
     @size.setter
-    def size(self, size):
-        self.width, self.height = size
+    def size(self, value):
+        """size setter"""
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
-        """ Assigns argument to each attribute. """
-        if args:
-            attributes = ("id", "width", "height", "x", "y")
-            for attribute in range(0, len(args)):
-                setattr(self, attributes[attribute], args[attribute])
-        else:
+        """update method"""
+        if args and args != 0:
+            count = 0
+            for arg in args:
+                if count == 0:
+                    if arg is None:
+                        super().__init__(self.width,
+                                         self.height, self.x,
+                                         self.y)
+                    else:
+                        self.id = arg
+                elif count == 1:
+                    self.size = arg
+                elif count == 2:
+                    self.x = arg
+                elif count == 3:
+                    self.y = arg
+                count += 1
+        elif kwargs and len(kwargs) != 0:
             for key, value in kwargs.items():
-                if hasattr(self, key):
-                    setattr(self, key, value)
+                if key == "id":
+                    if value is None:
+                        super().__init__(self.width,
+                                         self.height, self.x,
+                                         self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
 
-
-    def __str__(self):
-        """ overriding the __str__ method """
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id,
-            self.x,
-            self.y,
-            self.width,
-            self.height,
-        )
+    def to_dictionary(self):
+        """dictionary representation of self"""
+        return {"id": self.id, "size": self.size, "x": self.x,
+                "y": self.y}
