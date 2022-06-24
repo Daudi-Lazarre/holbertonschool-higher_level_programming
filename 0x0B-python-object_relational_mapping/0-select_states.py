e4b7398d88f23e10f5186292932b76bb7528e9d0
Daudi Lazarre <lucaslazarre@DaudiLazarre.local>
2022-06-24 16:47:37 -0400

    Create 0-select_states.py

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" List all of the states in the US """
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
