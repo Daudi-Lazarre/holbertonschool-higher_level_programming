d538ff1fc94d25db071440fa255535252a0c285b
Daudi Lazarre <77812330+daudi-lazarre@users.noreply.github.com>
2022-11-17 13:17:39 -0500

    Update 2-statuscode.js

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-const argv = process.argv;
-const URL = argv[2];
-
-request(URL, function (err, response) {
-    if (err) {
-        console.error('error:', error)
-    } else {
-        console.log(`code: ${response.statusCode}`);
-    }
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
