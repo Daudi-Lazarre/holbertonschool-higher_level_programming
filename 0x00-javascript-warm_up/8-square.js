7b601915dbc5c75d21cdacfa66276978197b8862
Daudi Lazarre <77812330+daudi-lazarre@users.noreply.github.com>
2022-11-17 13:09:08 -0500

    Update 8-square.js

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
-
-const val = Number(process.argv[2]);
-
-if (isNaN(val)) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < value; i++) {
-    let v = '';
-    for (let j = 0; j < value; j++) {
-      v += 'X';
-    }
-    console.log(v);
+const phrase = 'X';
+const x = process.argv[2];
+if (parseInt(x)) {
+  for (let iter = 0; iter < x; iter++) {
+    console.log(phrase.repeat(x));
   }
+} else {
+  console.log('Missing size');
 }
