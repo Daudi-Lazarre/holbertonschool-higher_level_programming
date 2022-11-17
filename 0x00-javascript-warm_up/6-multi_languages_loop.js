4effd833bad66783a52ee24585fa2eaf92941ef7
Daudi Lazarre <77812330+daudi-lazarre@users.noreply.github.com>
2022-11-17 13:20:58 -0500

    Update 6-multi_languages_loop.js

--- 0x00-javascript-warm_up/6-multi_languages_loop.js
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,16 @@
 #!/usr/bin/node
-const phrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
-while (i < phrases.length) {
-  console.log(phrases[i]);
-  i++;
-}
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
+  }
+});
