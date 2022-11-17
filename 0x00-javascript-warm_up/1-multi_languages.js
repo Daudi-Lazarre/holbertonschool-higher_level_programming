f31b7b436647a83e3fed10c08c5eba1aac7420d1
Daudi Lazarre <77812330+daudi-lazarre@users.noreply.github.com>
2022-11-17 13:06:21 -0500

    Update 1-multi_languages.js

--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-
-const lies = 'C is fun';
-const facts = 'Python is cool';
-const moreFacts = 'Javascript is amazing';
-
-console.log(lies);
-console.log(facts);
-console.log(moreFacts);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
