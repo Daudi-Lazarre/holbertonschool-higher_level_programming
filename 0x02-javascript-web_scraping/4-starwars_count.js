500e97a0ca51602996f6a482a96b70ba07bd7032
Daudi Lazarre <77812330+daudi-lazarre@users.noreply.github.com>
2022-11-17 13:18:47 -0500

    Update 4-starwars_count.js

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,12 @@
 #!/usr/bin/node
-const argv = process.argv;
-const URL = argv[2];
-const id = '18';
 const request = require('request');
-
-request(URL, function (error, response, body) {
-  if (error) {
-    console.error('error:', error);
-  } else {
-    const rbody = JSON.parse(body);
-    let count = 0;
-    for (const i of rbody.results) {
-      for (const j of i.characters) {
-        if (j.search(id) > 0) {
-          count++;
-        }
-      }
-    }
-    console.log(count);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
